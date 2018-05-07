390e600f39de3ed959b06a2ba96e0bbcee43c643
Alexander Nodia <alekonodia@gmail.com>
2018-05-07 19:21:36 +0400

    comment

--- includes/dbc.inc.php
+++ includes/dbc.inc.php
@@ -2,7 +2,7 @@
     $dbServername = "91.212.213.31";
     $dbUsername = "pepoge_user";
     $dbPassword = "bU63el6t3P";
-    $dbName = "pepoge_travel";
+    $dbName = "pepoge_tennis";
 
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
