7f48096899b46a1ee2dec12620077f3b0750f2f6
tura89 <turdzeladze@gmail.com>
2018-05-10 12:47:16 +0400

    MAP ADDED

--- mods/footer.mod.php
+++ mods/footer.mod.php
@@ -1,7 +1,6 @@
 <?php include "includes/get_generics.inc.php"; ?>
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d965.483649673611!2d44.74125384763843!3d41.721410625777324!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe33e59b57865d3a5!2sTop+Travel!5e0!3m2!1sen!2sge!4v1522770797373" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
-<footer class="page-footer text-md-left text-center">
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1487.8890937056062!2d44.786219333234754!3d41.7684399460061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sge!4v1525942012915" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe><footer class="page-footer text-md-left text-center">
     <div class="container">
         <div class="row">
             <div class="col-md-5">
